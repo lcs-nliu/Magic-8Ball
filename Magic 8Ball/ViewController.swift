0e40223f2aaa38bdf0e079d60960b32f4e099004
Nicole Liu <nicoleliu_@outlook.com>
2019-10-26 00:19:20 -0400

    Connected outlets and actions and finished the view, launch screen, and programmed a switch statement (did this all very fast so I forgot to commit)

--- Magic 8Ball/ViewController.swift
+++ Magic 8Ball/ViewController.swift
@@ -10,11 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Mark: Properties
+    // Generate random number between 1 and 20
+     let targetNumber = Int.random(in: 1...20)
+    @IBOutlet weak var generatedResponse: UILabel!
+    
+    
+    // Mark: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func shakeForResponse(_ sender: Any) {
+        switch targetNumber {
+        case 1:
+            generatedResponse.text = "It is certain"
+        case 2:
+            generatedResponse.text = "Without a doubt"
+        case 3:
+            generatedResponse.text = "You may rely on it"
+        case 4:
+            generatedResponse.text = "Yes definitely"
+        case 5:
+            generatedResponse.text = "It is decidedly so"
+        case 6:
+            generatedResponse.text = "As I see it, yes"
+        case 7:
+            generatedResponse.text = "Most likely"
+        case 8:
+            generatedResponse.text = "Yes"
+        case 9:
+            generatedResponse.text = "Outlook good"
+        case 10:
+            generatedResponse.text = "Signs point to yes"
+        case 11:
+            generatedResponse.text = "Reply hazy try again"
+        case 12:
+            generatedResponse.text = "Cannot predict now"
+        case 13:
+            generatedResponse.text = "Concentrate and ask again"
+        case 14:
+            generatedResponse.text = "Don’t count on it"
+        case 15:
+            generatedResponse.text = "Outlook not so good"
+        case 16:
+            generatedResponse.text = "My sources say no"
+        case 17:
+            generatedResponse.text = "Very doubtful"
+        case 18:
+            generatedResponse.text = "My reply is no"
+        case 19:
+            generatedResponse.text = "Better not tell you now"
+        case 20:
+            generatedResponse.text = "Ask again later"
+        default:
+            generatedResponse.text = "No response was generated. Please try again."
+        }
+    }
+    
 }
 
